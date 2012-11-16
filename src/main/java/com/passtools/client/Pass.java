209741d74bc06e5e71a431c386ca4085afeb4f91
teide <echeyde@tello.com>
2012-11-15 17:41:16 -0800

    adding pass.delete() and pass.viewpassbookjson

--- src/main/java/com/passtools/client/Pass.java
+++ src/main/java/com/passtools/client/Pass.java
@@ -172,5 +172,50 @@ public class Pass extends PassToolsClient {
 
     }
 
+    public static void delete(Long passId){
+
+        try {
+
+            checkNotNullPassId(passId);
+
+            String url = PassTools.API_BASE + "/pass/" + passId.toString();
+
+            PassToolsResponse response = delete(url);
+
+
+
+        } catch (RuntimeException rte) {
+            throw rte;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+    public static JSONObject viewPassbookJSONPass(Long passId){
+        try {
+
+            checkNotNullPassId(passId);
+
+            String url = PassTools.API_BASE + "/pass/" + passId.toString() +"/viewJSONPass";
+
+            PassToolsResponse response = get(url);
+
+            return response.getBodyAsJSONObject();
+
+
+
+        } catch (RuntimeException rte) {
+            throw rte;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 
 }
