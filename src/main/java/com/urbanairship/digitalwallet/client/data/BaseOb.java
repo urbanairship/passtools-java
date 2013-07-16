8ac685f0de30cbc8e78b236e00db2d9a75516a75
captainstupid <michael@tello.com>
2013-07-16 13:11:30 -0700

    preparing for release

--- src/main/java/com/urbanairship/digitalwallet/client/data/BaseOb.java
+++
@@ -1,9 +0,0 @@
-package com.urbanairship.digitalwallet.client.data;
-
-import org.json.simple.JSONObject;
-
-public interface BaseOb {
-    JSONObject toJSON();
-    void fromJSON(JSONObject json);
-    void fromJSON(String json);
-}
